af95aa3a2c0eb533a4d74169910cd216c84d86de
metehansargin <msargin1986@gmail.com>
2024-03-20 13:15:10 +0300

    templates update

---
+++ courseapp/tn/layout-2/contact.php
@@ -0,0 +1,13 @@
+<?php $title="iletişim";?>
+<?php     $content="views/_content.php"; 
+    $menu="views/_menu.php";
+    include 'layout.php';
+?>
+<main>
+    <h1>İletişim</h1>
+    <form action="">
+    <input type="text" name="" id="">
+    <input type="text" name="" id="">
+    <button type="sumbit">Gönder</button>
+    </form>
+</main>
---
+++ courseapp/tn/layout-2/views/_content.php
@@ -0,0 +1,5 @@
+<ul>
+    <li>Samsung s23</li>
+    <li>Sony erecsion</li>
+    <li>Iphone 15 Pro Max</li>
+</ul>
