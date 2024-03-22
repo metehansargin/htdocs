df4600baba3ccc96767e65c8f7728ecb40d79642
metehansargin <msargin1986@gmail.com>
2024-03-22 12:14:19 +0300

    ders

---
+++ courseapp/ders uygulama/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blue Rectangle</title>
+    <style>
+        .blue-rectangle {
+            background-color: #0000FF;
+            padding: 25px;
+            text-align: center;
+            box-sizing: border-box;
+        }
+        body{
+            background-color: #C0C0C0;
+        }
+        #kutu {
+            width:600px;
+            height:600px;
+            padding:5px;
+            color:#000;
+            text-align:justify;
+            position: absolute;
+            left: 20px;
+            margin: 250px 0;
+        }
+        #kutu2 {
+            width:200px;
+            height:100px;
+            padding:5px;
+            color:#000;
+            text-align:justify;
+            position: absolute;
+            right: 60px;
+            margin: 100px 0;
+        }
+        #buton{
+            width:200px;
+            height:100px;
+            padding:5px;
+            color:#000;
+            text-align:justify;
+            position: absolute;
+            right: 60px;
+            margin: 500px 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="blue-rectangle">
+    <h2 align="left">Cirsus Analysis</h2>
+    <form align="right"  action="">
+    <input height=""  type="text" name="" id="">
+    <input type="text" name="" id="">
+    <button  type="sumbit">Log In</button>
+    </form>
+
+    </div>
+
+    <div class="left-content" id="kutu">
+        Hello...Welcome to Circuit Analysis Platform... 
+        This platform serves tha wdjawdjköa mnsmdneıeıgknnmsfnsdmöfnwenfjnjajd
+        awdawdawdaw <pre></pre>
+        The ajwdnjawkdnjwaw dawkna awndjawnd awndjawndj awndjawnd jawndjawa
+        ajwndjawndjaw <pre></pre>
+        This platform serves tha wdjawdjköa mnsmdneıeıgknnmsfnsdmöfnwenfjnjajd
+        awdawdawdaw <pre></pre>
+        The ajwdnjawkdnjwaw dawkna awndjawnd awndjawndj awndjawnd jawndjawa
+        ajwndjawndjaw <pre></pre>
+
+    </div>
+    <div class="right-content" id="kutu2">
+        <h1>Sign Up</h1>
+        <h2>its quick and easy</h2>
+    </div>
+    <div class="" id="buton">
+    <input height="" type="text" name="" id=""><pre></pre>
+    <input height="" type="text" name="" id=""><pre></pre>
+    <input height="" type="text" name="" id=""><pre></pre>
+    <input height="" type="text" name="" id=""><pre></pre>
+    <br></br>
+    <button  type="sumbit">Log In</button>
+
+    </div>
+    
+</body>
+</html>
