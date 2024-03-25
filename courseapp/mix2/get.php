867522dc53019f555a896dc27f6eddf33c266cb7
metehansargin <msargin1986@gmail.com>
2024-03-25 20:29:56 +0300

    get forms

---
+++ courseapp/mix2/get.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>GET</title>
+</head>
+<body>
+    <form action="yazdir.php" method="GET">
+    <input type="text" name="q">
+    <input type="text" name="category">
+    <button type="submit" >Gönder</button>
+
+
+    </form>
+</body>
+</html>
