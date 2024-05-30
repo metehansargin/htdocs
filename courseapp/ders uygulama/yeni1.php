d36974e91e904a40083d3d452dcdd630709e4e7c
metehansargin <msargin1986@gmail.com>
2024-05-30 18:26:19 +0300

    update

--- courseapp/ders uygulama/yeni1.php
+++ courseapp/ders uygulama/yeni1.php
@@ -1,5 +1,26 @@
+
 <?php
-echo"Sayfanıza hosgeldiniz";
+ob_start();
+echo '<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <button type="submit" name="login">Log In</button>
+    </form>
+</body>
+</html>';
 
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
+    header("Location: index.php");
+    
+}
+echo '<a href="http://localhost/courseapp/ders%20uygulama/index.php">CİKİS YAP</a>';
+unset($_COOKIE['kullanici']);
+unset($_COOKIE['sifre']);
+ob_end_flush();
 ?>
